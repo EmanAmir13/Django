7303f011622c8c91f5e7f8b46d5c4bc5a6e0f320
Eman Amir <eman.amir@flicken.io>
2023-12-20 12:21:51 +0500

    Created File utilis and make new model Car

--- django-project/mysite/home/models.py
+++ django-project/mysite/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=18)
+    email = models.EmailField()
+    address = models.TextField()
+
+class Car(models.Model):
+    car_name=models.CharField(max_length=100)
+    speed=models.IntegerField(default=90)
+
+    def __str__(self) -> str:
+        return self.car_name
---
+++ django-project/mysite/home/utilis.py
@@ -0,0 +1,8 @@
+from home.models import Students
+
+import time
+
+def run_this_fun():
+    print("Function Startted")
+    time.sleep(1)
+    print("Function Excecuted")
