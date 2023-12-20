742a0ad53ad0559224c321c8151672be3f3f080b
Eman Amir <eman.amir@flicken.io>
2023-12-20 14:58:13 +0500

    Make a recipes functionand add the functionality that add the onject of model

--- recipe_project/vege/views.py
+++ recipe_project/vege/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 
 # Create your views here.
@@ -6,12 +7,16 @@ from django.shortcuts import render
 def recipes(request):
     if request.method == 'POST':
         data = request.POST
-        recipe_name=data.get('recipe_name')
-        recipe_description=data.get('recipe_description')
-        recipe_image=request.FILES.get('recipe_image')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+        recipe_image = request.FILES.get('recipe_image')
 
-        print(recipe_name)
-        print(recipe_description)
-        print(recipe_image)
+        Recipe.objects.create(
+            recipe_name=recipe_name,
+            recipe_description=recipe_description,
+            recipe_image=recipe_image
+        )
+
+        return redirect('/')
 
     return render(request, "index.html")
